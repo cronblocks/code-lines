55621d7db144f004f535e818850d47e7a8efd346
usama <mahmoodify@gmail.com>
2022-09-11 19:27:24 +0500

    adding props for desc

--- src/CodeLines.Lib/Types/FileResult.cs
+++ src/CodeLines.Lib/Types/FileResult.cs
@@ -6,5 +6,13 @@ namespace CodeLines.Lib.Types
 {
     public class FileResult
     {
+        public string Filename { get; set; }
+        
+        public Language Language { get; set; }
+
+        public ulong TotalLines { get; set; }
+        public ulong BlankLines { get; set; }
+        public ulong CommentLines { get; set; }
+        public ulong CodeLines { get; set; }
     }
 }
