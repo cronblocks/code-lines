0896af8888b4c107a0698d507c7609dd2da1ed0c
usama <mahmoodify@gmail.com>
2022-09-11 19:19:42 +0500

    adding summary info

--- src/CodeLines.Lib/Types/Result.cs
+++ src/CodeLines.Lib/Types/Result.cs
@@ -6,5 +6,9 @@ namespace CodeLines.Lib.Types
 {
     public class Result
     {
+        public Language Language { get; }
+        public ulong TotalLines { get; }
+        public ulong CommentLines { get; }
+        public ulong CodeLines { get; }
     }
 }
