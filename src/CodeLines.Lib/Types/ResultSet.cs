0f1f654533f75b4a299af671a51ef2503683069b
usama <mahmoodify@gmail.com>
2022-09-11 19:24:24 +0500

    adding ctor

--- src/CodeLines.Lib/Types/ResultSet.cs
+++ src/CodeLines.Lib/Types/ResultSet.cs
@@ -7,5 +7,10 @@ namespace CodeLines.Lib.Types
     public class ResultSet
     {
         public List<SummaryResult> SummaryResults { get; set; }
+
+        public ResultSet()
+        {
+            SummaryResults = new List<SummaryResult>();
+        }
     }
 }
