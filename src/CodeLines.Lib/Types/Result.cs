dcaa2554130b244931ffbd3e9be2c26cc27f71f9
usama <mahmoodify@gmail.com>
2022-09-11 19:20:00 +0500

    cleanup

--- src/CodeLines.Lib/Types/Result.cs
+++ src/CodeLines.Lib/Types/Result.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CodeLines.Lib.Types
+﻿namespace CodeLines.Lib.Types
 {
     public class Result
     {
         public Language Language { get; }
+
         public ulong TotalLines { get; }
         public ulong CommentLines { get; }
         public ulong CodeLines { get; }
