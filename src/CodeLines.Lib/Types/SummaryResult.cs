88ce1dfd4ccaf8eb5f7412f433cac8ab339b0311
usama <mahmoodify@gmail.com>
2022-09-11 19:27:59 +0500

    adding prop for blank lines

--- src/CodeLines.Lib/Types/SummaryResult.cs
+++ src/CodeLines.Lib/Types/SummaryResult.cs
@@ -5,6 +5,7 @@
         public Language Language { get; set; }
 
         public ulong TotalLines { get; set; }
+        public ulong BlankLines { get; set; }
         public ulong CommentLines { get; set; }
         public ulong CodeLines { get; set; }
     }
