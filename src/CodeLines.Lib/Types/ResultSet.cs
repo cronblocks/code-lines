a0239fa34110ee974f56fadcc53bc6c0b37080cc
usama <mahmoodify@gmail.com>
2022-09-11 19:29:14 +0500

    adding file-results placeholders

--- src/CodeLines.Lib/Types/ResultSet.cs
+++ src/CodeLines.Lib/Types/ResultSet.cs
@@ -5,10 +5,12 @@ namespace CodeLines.Lib.Types
     public class ResultSet
     {
         public List<SummaryResult> SummaryResults { get; set; }
+        public List<FileResult> FileResults { get; set; }
 
         public ResultSet()
         {
             SummaryResults = new List<SummaryResult>();
+            FileResults = new List<FileResult>();
         }
     }
 }
