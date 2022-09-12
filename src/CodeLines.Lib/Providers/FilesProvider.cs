6a6500df421c001674f5ec80e61252387b535b4f
usama <mahmoodify@gmail.com>
2022-09-12 18:35:13 +0500

    cleanup

--- src/CodeLines.Lib/Providers/FilesProvider.cs
+++ src/CodeLines.Lib/Providers/FilesProvider.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CodeLines.Lib.Providers
+﻿namespace CodeLines.Lib.Providers
 {
     internal class FilesProvider
     {
