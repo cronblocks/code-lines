08690de8fc36227a1aa0d35e11c19edf985075d0
usama <mahmoodify@gmail.com>
2022-09-13 19:14:38 +0500

    assigning to field

--- src/CodeLines.Lib/Providers/FilesProvider.cs
+++ src/CodeLines.Lib/Providers/FilesProvider.cs
@@ -2,9 +2,11 @@
 {
     internal class FilesProvider
     {
+        private readonly string dirname;
+
         public FilesProvider(string dirname)
         {
-
+            this.dirname = dirname;
         }
     }
 }
