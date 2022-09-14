6dfe010ee911d861062f2e0ddb531fab13b6aa7d
usama <mahmoodify@gmail.com>
2022-09-14 15:12:04 +0500

    adding exception class for representing error

---
+++ src/CodeLines.Lib/Exceptions/NeitherFileNorDirectoryException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeLines.Lib.Exceptions
+{
+    public class NeitherFileNorDirectoryException : Exception
+    {
+        public NeitherFileNorDirectoryException(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+}
