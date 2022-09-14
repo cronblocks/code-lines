9efe1329e6355e7c206af2e6e5241e10194dd9bc
usama <mahmoodify@gmail.com>
2022-09-14 15:17:36 +0500

    checking for file existence

--- src/CodeLines.Lib/Providers/TextLinesProvider.cs
+++ src/CodeLines.Lib/Providers/TextLinesProvider.cs
@@ -1,6 +1,21 @@
-﻿namespace CodeLines.Lib.Providers
+﻿using System;
+using System.IO;
+
+namespace CodeLines.Lib.Providers
 {
     internal class TextLinesProvider
     {
+        public TextLinesProvider(string filename)
+        {
+            Filename = filename;
+
+            if (!File.Exists(filename))
+            {
+                throw new FileNotFoundException(filename);
+            }
+        }
+
+        public string Filename { get; }
+        public ulong TotalLines { get; set; }
     }
 }
