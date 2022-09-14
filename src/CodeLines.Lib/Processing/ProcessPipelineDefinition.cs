bfb2eed4cd05ef8380a59ed186683c8bccc27d0f
usama <mahmoodify@gmail.com>
2022-09-14 20:08:28 +0500

    adding sample processing nodes

--- src/CodeLines.Lib/Processing/ProcessPipelineDefinition.cs
+++ src/CodeLines.Lib/Processing/ProcessPipelineDefinition.cs
@@ -1,10 +1,18 @@
-﻿namespace CodeLines.Lib.Processing
+﻿using CodeLines.Lib.Types;
+using System.Collections.Generic;
+
+namespace CodeLines.Lib.Processing
 {
     internal partial class ProcessPipeline
     {
+        private List<ProcessingNode> _processingNodes;
+
         private void CreatePipeline()
         {
+            _processingNodes = new List<ProcessingNode>();
 
+            _processingNodes.Add(new ProcessingNode(Language.C,         "c,h",       @"//", @"/*", @"*/", _logger));
+            _processingNodes.Add(new ProcessingNode(Language.CPlusPlus, "cpp,h,hpp", @"//", @"/*", @"*/", _logger));
         }
     }
 }
