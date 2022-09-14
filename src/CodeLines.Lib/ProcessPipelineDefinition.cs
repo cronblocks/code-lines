dda42e62efa55223fadc5d85a427b003afd636e0
usama <mahmoodify@gmail.com>
2022-09-14 19:27:36 +0500

    adding creation of pipeline

---
+++ src/CodeLines.Lib/ProcessPipelineDefinition.cs
@@ -0,0 +1,16 @@
+﻿using CodeLines.Lib.Providers;
+using CodeLines.Lib.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeLines.Lib
+{
+    internal partial class ProcessPipeline
+    {
+        private void CreatePipeline()
+        {
+            
+        }
+    }
+}
