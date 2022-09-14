bc361927fd961c6bfc788844acb6f32d0cc4e5c4
usama <mahmoodify@gmail.com>
2022-09-14 15:36:22 +0500

    adding message printing action method

--- src/CodeLines.Lib/LinesCounter.cs
+++ src/CodeLines.Lib/LinesCounter.cs
@@ -7,13 +7,18 @@ namespace CodeLines.Lib
 {
     public class LinesCounter
     {
-        public LinesCounter(string dir_or_filename, LogLevel logLevel = LogLevel.Info)
+        public LinesCounter(
+            string dir_or_filename,
+            Action<string> messagePrint,
+            LogLevel logLevel = LogLevel.Info)
         {
             DirOrFilename = dir_or_filename;
             LogLevel = logLevel;
+            MessagePrint = messagePrint;
         }
 
         public string DirOrFilename { get; }
+        public Action<string> MessagePrint { get; }
         public LogLevel LogLevel { get; }
     }
 }
