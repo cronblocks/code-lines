bff0f326eb4f350af23278bba34dbaf553df1d12
usama <mahmoodify@gmail.com>
2022-09-16 16:11:09 +0500

    updating func params

--- src/CodeLines.Lib/Processing/ProcessingNodeStateMachine.cs
+++ src/CodeLines.Lib/Processing/ProcessingNodeStateMachine.cs
@@ -14,7 +14,7 @@ namespace CodeLines.Lib.Processing
 
         }
 
-        private void UpdateStateMachine()
+        private void UpdateStateMachine(ref FileResult fr, string trimmedLine)
         {
 
         }
