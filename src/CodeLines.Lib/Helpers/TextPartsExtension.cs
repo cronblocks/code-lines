36f2d1cc93f95611d460a8d596da27fe2e98cbcb
usama <mahmoodify@gmail.com>
2022-09-16 17:17:26 +0500

    adding trimmed part before

--- src/CodeLines.Lib/Helpers/TextPartsExtension.cs
+++ src/CodeLines.Lib/Helpers/TextPartsExtension.cs
@@ -6,5 +6,17 @@ namespace CodeLines.Lib.Helpers
 {
     internal static class TextPartsExtension
     {
+        internal static string GetTrimmedPartBefore(this string original, string sep)
+        {
+            if (!string.IsNullOrEmpty(original))
+            {
+                if (original.Contains(sep))
+                {
+                    return original.Substring(0, original.IndexOf(sep)).Trim();
+                }
+            }
+
+            return original;
+        }
     }
 }
