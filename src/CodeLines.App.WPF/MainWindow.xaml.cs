2a783b80314598a847ba42386936599f4fb372f9
usama <mahmoodify@gmail.com>
2022-09-16 22:55:12 +0500

    adding winforms

--- src/CodeLines.App.WPF/MainWindow.xaml.cs
+++ src/CodeLines.App.WPF/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using WinForms = System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
