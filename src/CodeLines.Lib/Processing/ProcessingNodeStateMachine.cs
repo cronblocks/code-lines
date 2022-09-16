ff288bd0d5c3b0a91866f6553477e5200a270eef
usama <mahmoodify@gmail.com>
2022-09-16 16:26:44 +0500

    getting indices for comment patterns

--- src/CodeLines.Lib/Processing/ProcessingNodeStateMachine.cs
+++ src/CodeLines.Lib/Processing/ProcessingNodeStateMachine.cs
@@ -25,6 +25,25 @@ namespace CodeLines.Lib.Processing
             }
             else
             {
+                int singleLineCommentIndex = -1;
+                int multipleLineCommentStartIndex = -1;
+                int multipleLineCommentEndIndex = -1;
+
+                if (!string.IsNullOrEmpty(SingleLineCommentPattern) && trimmedLine.Contains(SingleLineCommentPattern))
+                {
+                    singleLineCommentIndex = trimmedLine.IndexOf(SingleLineCommentPattern);
+                }
+
+                if (!string.IsNullOrEmpty(MultipleLineCommentStartPattern) && trimmedLine.Contains(MultipleLineCommentStartPattern))
+                {
+                    multipleLineCommentStartIndex = trimmedLine.IndexOf(MultipleLineCommentStartPattern);
+                }
+
+                if (!string.IsNullOrEmpty(MultipleLineCommentEndPattern) && trimmedLine.Contains(MultipleLineCommentEndPattern))
+                {
+                    multipleLineCommentEndIndex = trimmedLine.IndexOf(MultipleLineCommentEndPattern);
+                }
+
                 switch (_smState)
                 {
                     case SMState.Normal:
