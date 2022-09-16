df98b8b5b22bc3187ec4314726c719384edac006
usama <mahmoodify@gmail.com>
2022-09-16 16:06:40 +0500

    adding state machine placeholder

---
+++ src/CodeLines.Lib/Processing/ProcessingNodeStateMachine.cs
@@ -0,0 +1,14 @@
+﻿using CodeLines.Lib.Helpers;
+using CodeLines.Lib.Providers;
+using CodeLines.Lib.Types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CodeLines.Lib.Processing
+{
+    internal partial class ProcessingNode
+    {
+        
+    }
+}
