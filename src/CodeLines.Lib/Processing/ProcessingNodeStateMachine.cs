7cf579b2919c4cc1e8e5178382c4fc5fc8b3a591
usama <mahmoodify@gmail.com>
2022-09-16 16:15:10 +0500

    adding state enum and initializing

--- src/CodeLines.Lib/Processing/ProcessingNodeStateMachine.cs
+++ src/CodeLines.Lib/Processing/ProcessingNodeStateMachine.cs
@@ -4,9 +4,17 @@ namespace CodeLines.Lib.Processing
 {
     internal partial class ProcessingNode
     {
+        private enum State
+        {
+            Normal,
+            CommentLines
+        }
+
+        private State _state;
+
         private void ResetStateMachine()
         {
-
+            _state = State.Normal;
         }
 
         private void UpdateStateMachine(ref FileResult fileResult, string trimmedLine)
