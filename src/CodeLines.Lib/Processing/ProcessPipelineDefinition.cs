040f75631b5f760856fadd306062b2f85ef890fe
usama <mahmoodify@gmail.com>
2022-09-17 18:48:29 +0500

    converting to list

--- src/CodeLines.Lib/Processing/ProcessPipelineDefinition.cs
+++ src/CodeLines.Lib/Processing/ProcessPipelineDefinition.cs
@@ -11,18 +11,21 @@ namespace CodeLines.Lib.Processing
         {
             _processingNodes = new List<ProcessingNode>();
 
-            _processingNodes.Add(new ProcessingNode(Language.ArduinoSketch,   "ino",           @"//",    @"/*",    @"*/",    _logger));
-            _processingNodes.Add(new ProcessingNode(Language.C,               "c,h",           @"//",    @"/*",    @"*/",    _logger));
-            _processingNodes.Add(new ProcessingNode(Language.CPlusPlus,       "cxx,cpp,h,hpp", @"//",    @"/*",    @"*/",    _logger));
-            _processingNodes.Add(new ProcessingNode(Language.CSharp,          "cs",            @"//",    @"/*",    @"*/",    _logger));
-            _processingNodes.Add(new ProcessingNode(Language.GitIgnore,       "gitignore",     @"#",     @"",      @"",      _logger));
-            _processingNodes.Add(new ProcessingNode(Language.HTML,            "htm,html",      @"",      @"<!--",  @"-->",   _logger));
-            _processingNodes.Add(new ProcessingNode(Language.Java,            "java",          @"//",    @"/*",    @"*/",    _logger));
-            _processingNodes.Add(new ProcessingNode(Language.Markdown,        "md",            @"",      @"<!--",  @"-->",   _logger));
-            _processingNodes.Add(new ProcessingNode(Language.Python,          "py",            @"#",     @"'''",   @"'''",   _logger));
-            _processingNodes.Add(new ProcessingNode(Language.PlainText,       "txt",           @"",      @"",      @"",      _logger));
-            _processingNodes.Add(new ProcessingNode(Language.XAML,            "xaml",          @"",      @"<!--",  @"-->",   _logger));
-            _processingNodes.Add(new ProcessingNode(Language.XML,             "xml",           @"",      @"<!--",  @"-->",   _logger));
+            _processingNodes.AddRange(
+                new ProcessingNode[] {
+                    new ProcessingNode(Language.ArduinoSketch,   "ino",           @"//",    @"/*",    @"*/",    _logger),
+                    new ProcessingNode(Language.C,               "c,h",           @"//",    @"/*",    @"*/",    _logger),
+                    new ProcessingNode(Language.CPlusPlus,       "cxx,cpp,h,hpp", @"//",    @"/*",    @"*/",    _logger),
+                    new ProcessingNode(Language.CSharp,          "cs",            @"//",    @"/*",    @"*/",    _logger),
+                    new ProcessingNode(Language.GitIgnore,       "gitignore",     @"#",     @"",      @"",      _logger),
+                    new ProcessingNode(Language.HTML,            "htm,html",      @"",      @"<!--",  @"-->",   _logger),
+                    new ProcessingNode(Language.Java,            "java",          @"//",    @"/*",    @"*/",    _logger),
+                    new ProcessingNode(Language.Markdown,        "md",            @"",      @"<!--",  @"-->",   _logger),
+                    new ProcessingNode(Language.Python,          "py",            @"#",     @"'''",   @"'''",   _logger),
+                    new ProcessingNode(Language.PlainText,       "txt",           @"",      @"",      @"",      _logger),
+                    new ProcessingNode(Language.XAML,            "xaml",          @"",      @"<!--",  @"-->",   _logger),
+                    new ProcessingNode(Language.XML,             "xml",           @"",      @"<!--",  @"-->",   _logger),
+                });
         }
 
         private ProcessingNode GetProcessingNode(string filename)
