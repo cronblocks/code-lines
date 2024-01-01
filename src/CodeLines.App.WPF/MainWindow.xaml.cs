96b8d17347900c464e3fa70004a9a5dbadead15a
usama <mahmoodify@gmail.com>
2024-01-02 02:20:41 +0500

    using file scoped ns

--- src/CodeLines.App.WPF/MainWindow.xaml.cs
+++ src/CodeLines.App.WPF/MainWindow.xaml.cs
@@ -5,111 +5,107 @@ using System.Threading;
 using System.Windows;
 using WinForms = System.Windows.Forms;
 
-namespace CodeLines.App.WPF
+namespace CodeLines.App.WPF;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private string _selectedPath = "";
+
+    private void OnFolderSelectionButton_Click(object sender, RoutedEventArgs e)
+    {
+        WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog()
         {
-            InitializeComponent();
-        }
+            AutoUpgradeEnabled = true,
+            Description = "Select file or directory",
+            UseDescriptionForTitle = true,
+            ShowNewFolderButton = false
+        };
 
-        private string _selectedPath = "";
-
-        private void OnFolderSelectionButton_Click(object sender, RoutedEventArgs e)
+        if (dialog.ShowDialog() == WinForms.DialogResult.OK)
         {
-            WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog()
-            {
-                AutoUpgradeEnabled = true,
-                Description = "Select file or directory",
-                UseDescriptionForTitle = true,
-                ShowNewFolderButton = false
-            };
-
-            if (dialog.ShowDialog() == WinForms.DialogResult.OK)
-            {
-                _selectedPath = dialog.SelectedPath;
-                pathTextBox.Text = _selectedPath;
-            }
+            _selectedPath = dialog.SelectedPath;
+            pathTextBox.Text = _selectedPath;
         }
+    }
 
-        private void OnClearButton_Click(object sender, RoutedEventArgs e)
+    private void OnClearButton_Click(object sender, RoutedEventArgs e)
+    {
+        outputTextBox.Text = "";
+    }
+
+    private void OnProcessButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(_selectedPath))
         {
-            outputTextBox.Text = "";
+            MessageBox.Show("Path is not provided", "Invalid Path");
         }
-
-        private void OnProcessButton_Click(object sender, RoutedEventArgs e)
+        else
         {
-            if (string.IsNullOrEmpty(_selectedPath))
-            {
-                MessageBox.Show("Path is not provided", "Invalid Path");
-            }
-            else
-            {
-                Thread thread = new Thread(ProcessThread);
-                thread.Start(_selectedPath);
-            }
+            Thread thread = new Thread(ProcessThread);
+            thread.Start(_selectedPath);
         }
+    }
 
-        private void ProcessThread(object? path)
-        {
-            string? targetPath = path as string;
+    private void ProcessThread(object? path)
+    {
+        string? targetPath = path as string;
 
-            if (!string.IsNullOrEmpty(targetPath))
-            {
-                RunOnGuiThread(() =>
-                {
-                    pathLabel.IsEnabled = false;
-                    pathTextBox.IsEnabled = false;
-                    folderSelectionButton.IsEnabled = false;
-                    clearButton.IsEnabled = false;
-                    processButton.IsEnabled = false;
-                });
-
-                try
-                {
-                    LinesCounter counter =
-                        new LinesCounter(targetPath, PrintOutputLine,
-                                         skippedDirOrFilenames: ".git,.svn,.vs,bin,obj");
-
-                    counter.Process();
-                    counter.PrintResult();
-                }
-                catch (NeitherFileNorDirectoryException ex)
-                {
-                    PrintOutputLine($"ERROR! Neither a file nor a directory: \"{ex.Name}\"");
-                }
-                catch (Exception ex)
-                {
-                    PrintOutputLine($"ERROR! \"{ex.Message}\"");
-                }
-
-                RunOnGuiThread(() =>
-                {
-                    pathLabel.IsEnabled = true;
-                    pathTextBox.IsEnabled = true;
-                    folderSelectionButton.IsEnabled = true;
-                    clearButton.IsEnabled = true;
-                    processButton.IsEnabled = true;
-                });
-            }
-        }
-
-        private void RunOnGuiThread(Action action)
-        {
-            mainWindow.Dispatcher.Invoke(action);
-        }
-
-        private void PrintOutputLine(string text)
+        if (!string.IsNullOrEmpty(targetPath))
         {
             RunOnGuiThread(() =>
             {
-                outputTextBox.AppendText($"{text}{Environment.NewLine}");
-                outputTextBox.ScrollToEnd();
+                pathLabel.IsEnabled = false;
+                pathTextBox.IsEnabled = false;
+                folderSelectionButton.IsEnabled = false;
+                clearButton.IsEnabled = false;
+                processButton.IsEnabled = false;
+            });
+
+            try
+            {
+                LinesCounter counter =
+                    new LinesCounter(targetPath, PrintOutputLine,
+                                     skippedDirOrFilenames: ".git,.svn,.vs,bin,obj");
+
+                counter.Process();
+                counter.PrintResult();
+            }
+            catch (NeitherFileNorDirectoryException ex)
+            {
+                PrintOutputLine($"ERROR! Neither a file nor a directory: \"{ex.Name}\"");
+            }
+            catch (Exception ex)
+            {
+                PrintOutputLine($"ERROR! \"{ex.Message}\"");
+            }
+
+            RunOnGuiThread(() =>
+            {
+                pathLabel.IsEnabled = true;
+                pathTextBox.IsEnabled = true;
+                folderSelectionButton.IsEnabled = true;
+                clearButton.IsEnabled = true;
+                processButton.IsEnabled = true;
             });
         }
+    }
+
+    private void RunOnGuiThread(Action action)
+    {
+        mainWindow.Dispatcher.Invoke(action);
+    }
+
+    private void PrintOutputLine(string text)
+    {
+        RunOnGuiThread(() =>
+        {
+            outputTextBox.AppendText($"{text}{Environment.NewLine}");
+            outputTextBox.ScrollToEnd();
+        });
     }
 }
